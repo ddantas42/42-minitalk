8034c95e2049e752faa512b56f08d5785be608c4
hiperDG <104842093+ddantas42@users.noreply.github.com>
2022-12-04 12:37:52 +0000

    Add files via upload

--- client.c
+++ client.c
@@ -3,21 +3,28 @@
 /*                                                        :::      ::::::::   */
 /*   client.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ddantas- <ddantas-@student.42.fr>          +#+  +:+       +#+        */
+/*   By: hiper <hiper@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/11 15:50:53 by ddantas-          #+#    #+#             */
-/*   Updated: 2022/08/12 16:21:45 by ddantas-         ###   ########.fr       */
+/*   Updated: 2022/12/04 12:34:54 by hiper            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-int main(int argc, char *argv[])
+int main(int argc, char *argv[3])
 {
-	int	n;
+	int	pid;
 
-	n = 10;
-	while (n-- >= 10)
-		ft_printf("N = %d | |argc = %d | argv = %s |FUNCIONA :D \n", n, argc, argv[0]);
+	if (!argv[1] || argc == 1)
+		return (-1);
+		
+	pid = atoi(argv[1]);
+
+	ft_printf("To: %d | Sent: %s\n", pid, argv[2]);
+
+	//signal(SIGUSR1, action);
+	//signal(SIGUSR2, action);
+	kill(pid, SIGUSR1);
 	return (0);
 }
--- server.c
+++ server.c
@@ -3,20 +3,38 @@
 /*                                                        :::      ::::::::   */
 /*   server.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ddantas- <ddantas-@student.42.fr>          +#+  +:+       +#+        */
+/*   By: hiper <hiper@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/11 15:42:09 by ddantas-          #+#    #+#             */
-/*   Updated: 2022/08/12 16:21:38 by ddantas-         ###   ########.fr       */
+/*   Updated: 2022/12/04 12:27:18 by hiper            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
+
+void handler(int sig)
+{
+	ft_printf("\nQuem te dera hehe: %d\n", sig);
+}
+
 int main(int argc, char *argv[])
 {
 	int	pid;
+	
+	struct sigaction sa;
+	sa.sa_handler = &handler;
+	sa.sa_flags = SA_RESTART;
+	
+	sigaction(SIGUSR1, &sa, NULL);
+	sigaction(SIGUSR2, &sa, NULL);
 
 	pid = getpid();
 
+	ft_printf("Server started\nPID = %d\n", pid);
+	ft_printf("Waiting signal...\n");
+
+	while (1)
+		pause();
 	return (pid);
 }
