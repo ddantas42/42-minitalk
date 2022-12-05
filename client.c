7e1af1c9c6be8fb48220175c5b3fe26ea69688c9
hiperDG <104842093+ddantas42@users.noreply.github.com>
2022-12-05 16:38:08 +0000

    Add files via upload

--- client.c
+++ client.c
@@ -3,51 +3,61 @@
 /*                                                        :::      ::::::::   */
 /*   client.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hiper <hiper@student.42.fr>                +#+  +:+       +#+        */
+/*   By: ddantas- <ddantas-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/11 15:50:53 by ddantas-          #+#    #+#             */
-/*   Updated: 2022/12/04 20:26:36 by hiper            ###   ########.fr       */
+/*   Updated: 2022/12/05 16:37:40 by ddantas-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-/*void str2bin(char *str)
+static	void	handler(int sig)
 {
-	int n;
-	char c;
-	int b;
+	ft_printf("TOU A CORRER HEHE\n");
+}
+
+
+static void	str2bin(int pid, char *str)
+{
+	int	n;
+	int	c;
+	int	b;
 
 	n = 0;
 	while (str[n])
 	{
-		c = str[n];
+		c = (int)str[n++];
 		b = 8;
 		while (b--)
 		{
-			
+			if (c % 2)
+				kill(pid, SIGUSR1);
+			else
+				kill(pid, SIGUSR2);
+			c /= 2;
+			usleep(100);
 		}
-		n++;
 	}
-}*/
+	b = 8;
+	while (b--)
+	{
+		kill(pid, SIGUSR2);
+		usleep(100);
+	}
+}
 
-int main(int argc, char *argv[3])
+int	main(int argc, char *argv[3])
 {
-	int	pid;
-
+	struct sigaction sa;
+	
+	sa.sa_handler = &handler;
+	sa.sa_flags = SA_RESTART;
+	
 	if (argc != 3 || !(ft_strlen(argv[2])))
 		return (-1);
-			
-	pid = atoi(argv[1]);
-
-	ft_printf("To: %d | Sent: %s\nchar = %c | binary = %d\n", pid, argv[2], 'a', 'a'<<1);
-
-	//str2bin(argv[2]);
-
-	kill(pid, SIGUSR1); // 10
-	sleep(1);
-	kill(pid, SIGUSR2); // 12
-
-	
+	ft_printf("To: %d | Sent: %s\n", atoi(argv[1]), argv[2]);
+	str2bin(atoi(argv[1]), argv[2]);
+	sigaction(SIGUSR1, &sa, NULL);
 	return (0);
 }
--- server.c
+++ server.c
@@ -3,37 +3,49 @@
 /*                                                        :::      ::::::::   */
 /*   server.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hiper <hiper@student.42.fr>                +#+  +:+       +#+        */
+/*   By: ddantas- <ddantas-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/11 15:42:09 by ddantas-          #+#    #+#             */
-/*   Updated: 2022/12/04 20:25:35 by hiper            ###   ########.fr       */
+/*   Updated: 2022/12/05 16:37:40 by ddantas-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
 
-void handler(int sig)
+static	void	handler(int sig)
 {
-	ft_printf("\nSig = %d\n", sig);
+	static int n = 0;
+	static unsigned char c = 0;
+	
+	if (sig == 10)
+		c += 128;
+	n++;
+	if (n == 8)
+	{
+		if (!c)
+		{
+			write(1, "\n", 2);
+			
+		}
+		write(1, &c, 1);
+		n = 0;
+		c = 0;
+	}
+	else 
+	c = c >> 1;
 }
 
-int main(int argc, char *argv[])
+int	main(int argc, char *argv[])
 {
-	int	pid;
-	
 	struct sigaction sa;
+	
 	sa.sa_handler = &handler;
 	sa.sa_flags = SA_RESTART;
-	
 	sigaction(SIGUSR1, &sa, NULL);
 	sigaction(SIGUSR2, &sa, NULL);
-
-	pid = getpid();
-
-	ft_printf("Server started\nPID = %d\n", pid);
+	ft_printf("Server started\nPID = %d\n", getpid());
 	ft_printf("Waiting signal...\n");
-
 	while (1)
 		pause();
 	return (0);
